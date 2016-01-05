ed1f9b4f3f282a736cf69ff1a9f48cd47b3afdeb
LucasLencinas <luucas.lencinas@gmail.com>
2016-01-05 18:59:08 +0000

    ya funciona el websocket de socketio. Solo un simple onConnection

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.io = require("socket.io")();
 
+/*-------------CONFIGURACION de la app-------------*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +24,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+/*--------FIN - CONFIGURACION de la app-------------*/
+
+app.io.on("connection", function(socket){
+  console.log("un usuario se conecto");
+})
+
+module.exports = app;
+
+
+
+
+
+
+
+/*
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +72,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
+*/
 
-module.exports = app;
